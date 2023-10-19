91b2105d12cc1c39bd9309718568f3fe3b82046e
Timur <storyphase@yandex.com>
2023-10-19 20:16:37 +0600

    fix: sort, schema

--- src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.item.model.Comment;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByItemId(long itemId);
